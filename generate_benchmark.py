277a006d4a76b2445189dc2e84d9defa954c22cc
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-29 14:28:18 +0200

    upgrade benchmark generation

--- generate_benchmark.py
+++ generate_benchmark.py
@@ -1,7 +1,8 @@
-from random import randint, choice, random
-from string import ascii_letters
+from random import randint, choice, random, randrange
+from string import ascii_letters, ascii_lowercase
 
 NUM_TESTCASES = 200_000_000
+
 
 def randomize_case(string):
     new_string = list(string)
@@ -11,37 +12,38 @@ def randomize_case(string):
         else:
             new_string[i] = new_string[i].upper()
 
-    return ''.join(new_string)
+    return "".join(new_string)
+
 
 def break_single_char(string):
+    """Replace single char in the first or last quater of the string."""
     length = len(string)
-    mid = length // 2
-    mid_char = string[mid]
-
-    # only replace letters, not chars like ',' etc.
-    i = 0
-    while mid_char not in ascii_letters:
-        i += 1
-        mid_char = string[mid+i]
-        
     new_string = list(string)
-    new_string[mid+i] = choice(list(set(ascii_letters) - set(mid_char)))
-    return ''.join(new_string)
+
+    if random() < 0.5:
+        idx = randrange(0, length // 4)
+    else:
+        idx = randrange(length // 4 * 3, length)
+
+    new_string[idx] = choice(list(set(ascii_lowercase) - set(new_string[idx].lower())))
+    return "".join(new_string)
+
 
 def random_string(length=None):
     if length is None:
         length = randint(20, 40)
 
-    return ''.join(choice(ascii_letters) for _ in range(length))
+    return "".join(choice(ascii_letters) for _ in range(length))
+
 
 def main():
     global NUM_TESTCASES
-    valid_palindromes = open('palindromes.txt').read().splitlines()
+    valid_palindromes = open("palindromes.txt").read().splitlines()
     l = len(valid_palindromes)
     generated_valid_palindromes = []
-   
+
     idx = 0
-    while NUM_TESTCASES > 0: 
+    while NUM_TESTCASES > 0:
         palindrome = valid_palindromes[idx]
         print(palindrome)
         generated_valid_palindromes.append(palindrome)
@@ -54,9 +56,10 @@ def main():
         NUM_TESTCASES -= 100
         idx = (idx + 1) % l
 
-    with open('expected_palindromes.txt' ,'w') as f:
+    with open("expected_palindromes.txt", "w") as f:
         for p in generated_valid_palindromes:
-            f.write(p + '\n')
+            f.write(p + "\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
